c7387ab6fd4a0c72da16575c2838a8b9b105dbfb
Lisbeth <13511707@qq.com>
2021-11-19 01:23:51 +0800

    创建 <Layout />

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,7 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-            <p>money.vue</p>
-        </div>
-        <Nav />
-    </div>
+    <Layout>
+        <p>Money.vue</p>
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -14,15 +11,4 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-      border: 1px solid green;
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-  }
-  .content {
-      overflow: auto;
-      flex-grow: 1;
-      border: 1px solid blue;
-  }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,7 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-            <p>statistics.vue</p>
-        </div>
-        <Nav />
-    </div>
+    <Layout>
+        <p>Statistics.vue</p>
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -14,15 +11,4 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-      border: 1px solid green;
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-  }
-  .content {
-      overflow: auto;
-      flex-grow: 1;
-      border: 1px solid blue;
-  }
 </style>
