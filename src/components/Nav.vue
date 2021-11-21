96c75efa5eff1361acea8f44c00de52bea654820
Lisbeth <13511707@qq.com>
2021-11-21 23:47:55 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="nav">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money" />
-      <router-link to="/money">记账</router-link>
-      |
+      记账
+    </router-link>
+    <router-link to="/labels" class="item">
       <Icon name="labels" />
-      <router-link to="/labels">标签</router-link>
-      |
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
       <Icon name="statistics" />
-      <router-link to="/statistics">统计</router-link>
-    </div>
+      统计
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
-  export default {
-      name: 'Nav'
-  }
+export default {
+  name: "Nav",
+};
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid red;
+nav {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  font-size: 12px;
+  > .item {
+    width: 33.33333%;
+    padding: 2px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > .icon {
+      width: 32px;
+      height: 32px;;
+    }
   }
+}
 </style>
