818d98f1d312c4d0be834112c5398e663e9ec7ed
Lisbeth <13511707@qq.com>
2021-12-26 02:31:27 +0800

    制作返回按钮

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left" />
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
@@ -57,6 +57,10 @@ export default class EditLabel extends Vue {
     if (this.tag && this.tag.id) {
       tagListModel.remove(this.tag.id);
     }
+  }
+
+  goBack(){
+    this.$router.back()
   }
 }
 </script>
