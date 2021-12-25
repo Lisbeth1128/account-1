5e88d421b23483317a33b25b712b46d685ffa680
Lisbeth <13511707@qq.com>
2021-12-26 02:12:27 +0800

    可以删除标签，但是好像有连续删除 bug

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,12 +6,15 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" 
-                @update:value="update"
-                field-name="标签名" placeholder="请输入标签名" />
+      <FormItem
+        :value="tag.name"
+        @update:value="update"
+        field-name="标签名"
+        placeholder="请输入标签名"
+      />
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -30,7 +33,7 @@ import Button from "@/components/Button.vue";
   },
 })
 export default class EditLabel extends Vue {
-  tag? : {id: string, name: string} = undefined
+  tag?: { id: string; name: string } = undefined;
 
   created() {
     const id = this.$route.params.id;
@@ -38,15 +41,21 @@ export default class EditLabel extends Vue {
     const tags = tagListModel.data;
     const tag = tags.filter((t) => t.id === id)[0];
     if (tag) {
-      this.tag = tag
+      this.tag = tag;
     } else {
       this.$router.replace("/NotFound");
     }
   }
 
   update(name: string) {
-    if(this.tag){
-      tagListModel.update(this.tag.id, name)
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if (this.tag && this.tag.id) {
+      tagListModel.remove(this.tag.id);
     }
   }
 }
