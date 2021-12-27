28223f3ff1480c41a46702ae4f22a16798de1854
Lisbeth <13511707@qq.com>
2021-12-28 02:48:12 +0800

    将 create 封装到 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,16 +1,22 @@
-const localStorageKeyName = 'recordList'
-const recordListModel = {
-    data: [] as RecordItem[],
-    clone(data: RecordItem[] | RecordItem) {
-        return JSON.parse(JSON.stringify(data))
-    },
-    fetch(){
-        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[]
-        return this.data
-    },
-    save(){
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
-    }
-}
+import clone from '@/lib/clone'
 
-export default recordListModel
+const localStorageKeyName = "recordList";
+const recordListModel = {
+  data: [] as RecordItem[],
+  create(record: RecordItem) {
+    const record2: RecordItem = clone(this.data);
+    record2.createdAt = new Date();
+    this.data.push(record2);
+  },
+  fetch() {
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    ) as RecordItem[];
+    return this.data;
+  },
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+  },
+};
+
+export default recordListModel;
